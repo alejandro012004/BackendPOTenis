9a102af7b863aa8e28bf2654fd4dc20985ba8dc9
alejandro012004 <150953789+alejandro012004@users.noreply.github.com>
2025-12-14 19:08:50 +0100

    Add files via upload

---
+++ src/v1/rutas/partidoRutas.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const partidoControlador = require("../../controlador/partidoControlador");
+
+router.get("/actual", partidoControlador.obtenerPartidoActual);
+router.post("/guardar", partidoControlador.guardarPartido);
+router.post("/finalizar", partidoControlador.finalizarPartido); // NUEVA RUTA
+router.get("/proximos", partidoControlador.obtenerPartidosFuturos);
+router.get("/estado_global", partidoControlador.obtenerEstadoGlobal);
+
+router.get("/admin/jugadores", partidoControlador.obtenerJugadores);
+router.patch("/admin/jugadores/:id", partidoControlador.actualizarDatosJugador);
+router.patch("/admin/configuracion", partidoControlador.actualizarConfiguracion);
+
+module.exports = router;
